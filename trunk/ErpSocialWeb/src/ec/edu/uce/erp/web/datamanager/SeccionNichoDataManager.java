f951cc82f4f5663da04c718932f80e996b859547
ERPSOCIAL2014@gmail.com <ERPSOCIAL2014@gmail.com@d40b8d87-27a8-11df-1e59-73a91ceea785>
2014-12-05 03:53:34 +0000

    seccion nicho datamanagerimplementacion

--- trunk/ErpSocialWeb/src/ec/edu/uce/erp/web/datamanager/SeccionNichoDataManager.java
+++ trunk/ErpSocialWeb/src/ec/edu/uce/erp/web/datamanager/SeccionNichoDataManager.java
@@ -1,17 +1,62 @@
 package ec.edu.uce.erp.web.datamanager;
+import java.util.ArrayList;
+import java.util.List;
 
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+import ec.edu.uce.erp.ejb.persistence.entity.eucaristia.CatalogoEucaristiaDTO;
 import ec.edu.uce.erp.web.common.datamanager.BaseDataManager;
 
 @SessionScoped
 @ManagedBean (name = "seccionNichoDataManager")
 public class SeccionNichoDataManager extends BaseDataManager{
 
-//private static final Logger slf4jLogger = LoggerFactory.getLogger(SeccionNichoDataManager.class);
+private static final Logger slf4jLogger = LoggerFactory.getLogger(SeccionNichoDataManager.class);
 	
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
+
+private CatalogoEucaristiaDTO seccionNichoInsertar;
+private List<CatalogoEucaristiaDTO> seccionNichoDTOs;
+private CatalogoEucaristiaDTO seccionNichoBuscar;
+
+@PostConstruct
+public void inicializarObjetos () {
+	slf4jLogger.info("inicializarObjetos");
+	this.seccionNichoDTOs=new ArrayList<CatalogoEucaristiaDTO>();
+	this.seccionNichoInsertar=new CatalogoEucaristiaDTO();
+	this.seccionNichoBuscar=new CatalogoEucaristiaDTO();
+
+}
+
+public CatalogoEucaristiaDTO getSeccionNichoInsertar() {
+	return seccionNichoInsertar;
+}
+
+public void setSeccionNichoInsertar(CatalogoEucaristiaDTO seccionNichoInsertar) {
+	this.seccionNichoInsertar = seccionNichoInsertar;
+}
+
+public List<CatalogoEucaristiaDTO> getSeccionNichoDTOs() {
+	return seccionNichoDTOs;
+}
+
+public void setSeccionNichoDTOs(List<CatalogoEucaristiaDTO> seccionNichoDTOs) {
+	this.seccionNichoDTOs = seccionNichoDTOs;
+}
+
+public CatalogoEucaristiaDTO getSeccionNichoBuscar() {
+	return seccionNichoBuscar;
+}
+
+public void setSeccionNichoBuscar(CatalogoEucaristiaDTO seccionNichoBuscar) {
+	this.seccionNichoBuscar = seccionNichoBuscar;
+}
 	
 	
 
